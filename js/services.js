6bf083d2e7420b3dcdf70acfdbf81ad2ad19bf6e
Claire Parker <claire.parker87@gmail.com>
2015-10-27 09:51:27 +0000

    :surfer: Clues are dynamically shuffled

--- js/services.js
+++ js/services.js
@@ -1,70 +1,44 @@
 var services = angular.module('AnagramApp.services', [])
 	.factory('AnagramFactory', [function AnagramFactory() {
-		allAnagrams = [
-		 {
-		    "clue": [
-		        {
-		            "letter": "I"
-		        },
-		        {
-		            "letter": "W"
-		        },
-		        {
-		            "letter": "N"
-		        }
-		    ],
-		    "solution": "WIN"
-		},
-		{
-		    "clue": [
-		        {
-		            "letter": "E"
-		        },
-		        {
-		            "letter": "P"
-		        },
-		        {
-		            "letter": "L"
-		        },
-		        {
-		            "letter": "P"
-		        },
-		        {
-		            "letter": "A"
-		        }
-		    ],
-		    "solution": "APPLE"
-		},
-		{
-		    "clue": [
-		        {
-		            "letter": "I"
-		        },
-		        {
-		            "letter": "C"
-		        },
-		        {
-		            "letter": "T"
-		        },
-		        {
-		            "letter": "U"
-		        },
-		        {
-		            "letter": "I"
-		        },
-		        {
-		            "letter": "B"
-		        },
-		        {
-		            "letter": "S"
-		        }
-		    ],
-		    "solution": "BISCUIT"
-		}
-		];
+
+		var allWords = ["BISCUIT", "GRANOLA", "JELLY", "FUSILLI", "ORANGE"];
+
+		var makeAnagramObj = function(word) {
+			var letterArr = [];
+			var shuffledArr = shuffleWord(word);
+			for (var i = 0; i < word.length; i++) {
+				letterArr.push({"letter": shuffledArr[i]});
+			};
+			return {
+				"clue": letterArr,
+				"solution": word
+			};
+		};
+
+		// Thanks StackOverflow http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+		var shuffleWord = function (word) {
+			var array = word.split('');
+		  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+		  // While there remain elements to shuffle...
+		  while (0 !== currentIndex) {
+
+		    // Pick a remaining element...
+		    randomIndex = Math.floor(Math.random() * currentIndex);
+		    currentIndex -= 1;
+
+		    // And swap it with the current element.
+		    temporaryValue = array[currentIndex];
+		    array[currentIndex] = array[randomIndex];
+		    array[randomIndex] = temporaryValue;
+		  }
+
+		  return array;
+		};
+
 		return {
 			getAnagram: function() {
-				return allAnagrams[Math.floor(Math.random() * allAnagrams.length)];
+				return makeAnagramObj(allWords[Math.floor(Math.random() * allWords.length)]);
 			}
 		};
 	}]);
